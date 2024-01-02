67d3235f8ed33036b1f9abcaf2fb7a3afb990f03
christian_reynaltt <creynaltt@maristak.net>
2024-01-02 20:39:07 +0100

    se abstrae el readJSON

---
+++ utils.js
@@ -0,0 +1,5 @@
+//como leer un json en ESModules de forma recomendada por ahora
+import { createRequire } from 'node:module'
+const require = createRequire(import.meta.url)
+
+export const readJSON = (path) => require(path)
